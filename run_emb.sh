61cb2ea4076702fd421a28b1989371c35811bcbd
Zhuofeng Li <zhuofengli12345@gmail.com>
2024-10-27 05:43:21 +0000

    update

--- run_emb.sh
+++ run_emb.sh
@@ -1,46 +1,66 @@
 #!/bin/bash
 #!/bin/bash
 
-# Define an array of dataset configurations
-datasets=(
-    "goodreads_children:children"
-    "goodreads_history:history"
-    "goodreads_comics:comics"
-    "goodreads_crime:crime"
-    # "twitter:twitter"
-    "amazon_movie:movie"
-    "amazon_baby:baby"
-    "reddit:reddit"
-)
+# # Define an array of dataset configurations
+# datasets=(
+#     "goodreads_children:children"
+#     "goodreads_history:history"
+#     "goodreads_comics:comics"
+#     "goodreads_crime:crime"
+#     # "twitter:twitter"
+#     # "amazon_movie:movie"
+#     # "amazon_baby:baby"
+#     # "reddit:reddit"
+# )
 
-# Loop through each dataset
-for dataset in "${datasets[@]}"; do
-    # Split the dataset string into folder and name
-    IFS=":" read -r folder name <<< "$dataset"
+# # Loop through each dataset
+# for dataset in "${datasets[@]}"; do
+#     # Split the dataset string into folder and name
+#     IFS=":" read -r folder name <<< "$dataset"
     
-    echo "Processing dataset: $folder"
+#     echo "Processing dataset: $folder"
     
-    python data_preprocess/generate_llm_emb.py \
-        --pkl_file "Dataset/$folder/processed/${name}.pkl" \
-        --path "Dataset/$folder/emb" \
-        --name "$name" \
-        --model_name meta-llama/Meta-Llama-3-8B \
-        --max_length 2048 \
-        --batch_size 25
-done
+#     python data_preprocess/generate_llm_emb.py \
+#         --pkl_file "Dataset/$folder/processed/${name}.pkl" \
+#         --path "Dataset/$folder/emb" \
+#         --name "$name" \
+#         --model_name meta-llama/Meta-Llama-3-8B \
+#         --max_length 2048 \
+#         --batch_size 25
+# done
 
 
 # python data_preprocess/generate_llm_emb.py \
-#     --pkl_file Dataset/goodreads_history/processed/history.pkl \
-#     --path Dataset/goodreads_history/emb \
-#     --name history \
+#     --pkl_file Dataset/arxiv/processed/arxiv.pkl \
+#     --path Dataset/arxiv/emb \
+#     --name arxiv \
 #     --model_name bert-base-uncased \
+
+# python data_preprocess/generate_llm_emb.py \
+#     --pkl_file Dataset/arxiv/processed/arxiv.pkl \
+#     --path Dataset/arxiv/emb \
+#     --name arxiv \
+#     --model_name bert-large-uncased \
+#     --batch_size 250
  
 # python data_preprocess/generate_llm_emb.py \
 #     --pkl_file Dataset/twitter/processed/twitter.pkl \
 #     --path Dataset/twitter/emb \
 #     --name tweets \
 #     --model_name bert-base-uncased \
+
+python data_preprocess/generate_llm_emb.py \
+    --pkl_file Dataset/twitter/processed/twitter.pkl \
+    --path Dataset/twitter/emb \
+    --name tweets \
+    --model_name bert-base-uncased \
+
+python data_preprocess/generate_llm_emb.py \
+    --pkl_file Dataset/twitter/processed/twitter.pkl \
+    --path Dataset/twitter/emb \
+    --name tweets \
+    --model_name bert-large-uncased \
+    --batch_size 250
 
 # python data_preprocess/generate_llm_emb.py \
 #     --pkl_file Dataset/amazon_movie/processed/movie.pkl \
